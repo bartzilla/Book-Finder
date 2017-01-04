58e04d3cfef763444dad3f54a0626a18e6b0835f
Cipriano Sanchez <ciprianosanchez@gmail.com>
2017-01-04 16:56:22 +0100

    unnecessary comments

--- client/app/book-details-modal/book-details-modal.controller.js
+++ client/app/book-details-modal/book-details-modal.controller.js
@@ -1,8 +1,4 @@
 angular.module('booksApp')
   .controller('BookDetailsCtrl', function ($scope) {
-    // $scope.showModal = function(){
-    //   $dialog.dialog({}).open('book-details-modal.html');
-    // }
-
     console.log('Hey from BookDetailsCtrl');
   });
